13c4ebbc15e04f3a11d82d460f2e95f1852f97c6
Yuval Adam <yuv.adm@gmail.com>
2012-11-16 11:41:25 +0200

    Celery schedule and print fib task

--- tasks.py
+++ tasks.py
@@ -1,11 +1,26 @@
+import logging
+
 from celery import Celery
+from celery.schedules import crontab
 
 celery = Celery('tasks', broker='redis://localhost')
 
+CELERYBEAT_SCHEDULE = {
+    'every-ten-secs': {
+        'task': 'tasks.print_fib',
+        'schedule': crontab(minute='*/1'),
+        'args': (30),
+    },
+}
 
-@celery.task
+
 def fib(n):
     if n > 1:
         return fib(n - 1) + fib(n - 2)
     else:
         return 1
+
+
+@celery.task
+def print_fib(n):
+    logging.info(fib(n))
