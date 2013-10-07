5b03619a03747833e149aa6f304a5dfd18a756b6
Yuval Adam <yuv.adm@gmail.com>
2013-10-07 14:25:59 +0200

    Add some descriptive comments

--- tasks.py
+++ tasks.py
@@ -5,18 +5,21 @@ from celery.task import periodic_task
 from datetime import timedelta
 from os import environ
 
+# Fetch the Redis connection string from the env, or use localhost by default
 REDIS_URL = environ.get('REDISTOGO_URL', 'redis://localhost')
 
+# Setup the celery instance under the 'tasks' namespace
 celery = Celery('tasks', broker=REDIS_URL)
 
-
+# Define the fibonacci function for use in our task
 def fib(n):
     if n > 1:
         return fib(n - 1) + fib(n - 2)
     else:
         return 1
 
-
+# The periodic task itself, defined by the following decorator
 @periodic_task(run_every=timedelta(seconds=10))
 def print_fib():
+    # Just log fibonacci(30), no more
     logging.info(fib(30))
